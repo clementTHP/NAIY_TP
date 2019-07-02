27c98f099fdaa03ca0c1404ca9d02c4ce46eac51
clementTHP <52445157+clementTHP@users.noreply.github.com>
2019-07-02 15:27:09 +0200

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+  <title>Google</title>
+    <style>
+        #box
+        {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 650px;
+        }
+        #box1
+        {
+            display: flex;
+            justify-content: flex-end;
+        }
+        #box2
+        {
+            display: flex;
+            justify-content: center;
+        }
+        #box2b
+        {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-center;
+            
+        }
+        #box3
+        {
+            display: flex
+            justify-content: space-center;
+        }
+        #block
+        {
+            background-color: none;
+            width: 700px;
+            height: 60px;
+            border-radius: 30px;
+
+            border-width:1px;
+            border-style:dotted;
+            border-color:black;
+        }
+        
+    </style>
+<body>
+  <div id="box"> 
+      <div id="box1">
+          <div>
+              <a  href="https://mail.google.com/mail/?tab=wm0&amp;ogbl" data-pid="23">Gmail</a>
+              <a  href="https://www.google.fr/imghp?hl=fr&amp;tab=wi0&amp;ogbl" data-pid="2">Images</a>
+              <a tabindex="0" title="Applications Google" role="button" aria-expanded="false" href="https://www.google.fr/intl/fr/about/products?tab=wh0"></a>
+              <button>
+              <a href="https://accounts.google.com/ServiceLogin?hl=fr&amp;passive=true&amp;continue=https://www.google.com/" target="_top">Connexion</a></button>
+          </div>
+      </div>
+      <div id="box2">
+          <div id="box2b">
+              <div> 
+                  <center> <B> <h1> <FONT size="10Pt" family="arial,sans-serif"> 
+                      <FONT color="blue">G</FONT>
+                      <FONT color="red">O</FONT>
+                      <FONT color="yellow">O</FONT>
+                      <FONT color="blue">G</FONT>
+                      <FONT color="red">L</FONT>
+                      <FONT color="red">E</FONT>
+                  </FONT> </h1> </B> </center>
+              </div>
+              <div id="block">
+                  <center><br>
+                      <input type="text" id="rch" name="rch" required minlength="1" maxlength="500" size="100" placeholder="What is THP ?"  margin-top="5px" margin-right="5px" border-color="blank">
+                  </center>
+              </div>
+              <div><br><br><center>
+              <button>
+              <a href="https:0" target="_top">Recherche Google</a></button>
+              <button>
+              <a href="https:0" target="_top">J'ai de la chance</a></button>
+                 
+              </center></div>
+          </div>
+      </div>
+      <div id="box3">
+            <div> </div>
+        </div>
+    </div>
+</body>
+</html>
